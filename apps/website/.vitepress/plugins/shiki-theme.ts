e67581527eec29316b0b781ebaac5285bdce7e02
christopher-buss <christopher.buss+github@protonmail.com>
2026-05-12 00:27:54 +0100

    feat(website): update bedrock warm theme

--- apps/website/.vitepress/plugins/shiki-theme.ts
+++ apps/website/.vitepress/plugins/shiki-theme.ts
@@ -9,20 +9,43 @@ export const BEDROCK_WARM: ThemeRegistration = {
 	name: "bedrock-warm",
 	colors: {
 		"editor.background": "#0f1320",
-		"editor.foreground": "#dcd7ca",
+		"editor.foreground": "#b9c1d0",
+		"editor.lineHighlightBackground": "#161c2c",
+		"editor.selectionBackground": "#1e2434",
+		"editorBracketMatch.background": "#1e2434",
+		"editorBracketMatch.border": "#7a9ac4",
+		"editorCursor.foreground": "#eef1f7",
+		"editorIndentGuide.background1": "#1e2434",
+		"editorLineNumber.activeForeground": "#b9c1d0",
+		"editorLineNumber.foreground": "#6f7889",
+		"terminal.background": "#080b12",
+		"terminal.foreground": "#b9c1d0",
 	},
+	displayName: "Bedrock Warm",
+	semanticHighlighting: true,
 	tokenColors: [
 		{
-			scope: ["comment", "punctuation.definition.comment"],
+			scope: ["comment", "punctuation.definition.comment", "string.comment"],
 			settings: { fontStyle: "italic", foreground: "#6f7889" },
+		},
+		{
+			scope: ["string", "string.quoted", "string.template"],
+			settings: { foreground: "#b8d4a8" },
+		},
+		{
+			scope: ["constant.numeric", "constant.language.boolean", "constant.language.null"],
+			settings: { foreground: "#d4a878" },
 		},
 		{
 			scope: [
 				"keyword",
 				"keyword.control",
 				"keyword.operator.new",
+				"keyword.operator.expression",
 				"storage.type",
 				"storage.modifier",
+				"variable.language.this",
+				"variable.language.self",
 			],
 			settings: { foreground: "#c88a7a" },
 		},
@@ -36,32 +59,59 @@ export const BEDROCK_WARM: ThemeRegistration = {
 			settings: { foreground: "#dcc8a8" },
 		},
 		{
-			scope: ["string", "punctuation.definition.string"],
-			settings: { foreground: "#b8d4a8" },
-		},
-		{
-			scope: ["constant.numeric", "constant.language"],
-			settings: { foreground: "#d4a878" },
-		},
-		{
 			scope: [
 				"meta.object-literal.key",
-				"variable.other.property",
 				"support.type.property-name",
+				"variable.other.property",
+				"meta.property-name",
 			],
 			settings: { foreground: "#e6b472" },
 		},
 		{
-			scope: ["entity.name.type", "support.type", "support.class"],
+			scope: [
+				"entity.name.type",
+				"entity.name.class",
+				"entity.name.interface",
+				"support.type",
+				"support.class",
+			],
 			settings: { foreground: "#a8b8c8" },
 		},
 		{
-			scope: ["punctuation", "meta.brace", "meta.delimiter"],
+			scope: ["punctuation", "meta.brace", "punctuation.separator", "punctuation.terminator"],
 			settings: { foreground: "#6f7889" },
 		},
 		{
 			scope: ["variable", "variable.other"],
-			settings: { foreground: "#dcd7ca" },
+			settings: { foreground: "#b9c1d0" },
+		},
+		{
+			scope: ["constant", "constant.character", "constant.other"],
+			settings: { foreground: "#d4a878" },
+		},
+		{
+			scope: ["entity.other.attribute-name"],
+			settings: { foreground: "#e6b472" },
+		},
+		{
+			scope: ["entity.name.tag"],
+			settings: { foreground: "#c88a7a" },
+		},
+		{
+			scope: ["markup.heading"],
+			settings: { foreground: "#eef1f7" },
+		},
+		{
+			scope: ["markup.bold"],
+			settings: { fontStyle: "bold", foreground: "#eef1f7" },
+		},
+		{
+			scope: ["markup.italic"],
+			settings: { fontStyle: "italic", foreground: "#b9c1d0" },
+		},
+		{
+			scope: ["markup.inline.raw", "markup.fenced_code"],
+			settings: { foreground: "#b8d4a8" },
 		},
 	],
 	type: "dark",
